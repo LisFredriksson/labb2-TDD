44ea872d58d394b10b71cb82ca65e32ff88e857c
Lis Fredriksson <lisfredrikson@gmail.com>
2024-01-02 11:42:05 +0100

    validate ZIP test

---
+++ src/__tests__/validate-ZIP.spec.ts
@@ -0,0 +1,23 @@
+import { validateZIP } from '../validations/validateZIP';
+
+describe('validate-Zip.ts tests', () => {
+  it('should return true for a valid zip code "12345"', () => {
+    const result = validateZIP('12345');
+    expect(result).toBe(true);
+  });
+
+  it('should return false for an invalid zip code "1234"', () => {
+    const result = validateZIP('1234');
+    expect(result).toBe(false);
+  });
+
+  it('should return false for an invalid zip code "123456"', () => {
+    const result = validateZIP('123456');
+    expect(result).toBe(false);
+  });
+
+  it('should return false for a non-numeric zip code "abcde"', () => {
+    const result = validateZIP('abcde');
+    expect(result).toBe(false);
+  });
+});
