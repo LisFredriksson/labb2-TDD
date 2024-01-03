29cb2a7e525763f448988076def26cd7658f0e0d
Lis Fredriksson <lisfredrikson@gmail.com>
2024-01-03 13:43:01 +0100

    updated test for GET /contact and added test for POST /contact

--- src/__tests__/server.spec.ts
+++ src/__tests__/server.spec.ts
@@ -1,14 +1,44 @@
 import request from 'supertest';
 import { app } from '../app';
 
-describe('app.ts', () => {
-    it("should return json", () => {
-        request(app)
-        .get('/contact')
-        .expect('Content-Type', /json/)
-        .expect(200)
-        .end(function (err, res) {
-            if (err) throw err;
-        })
-    })
+describe('GET /contact', () => {
+  it('should return json and status code 200', (done) => {
+    request(app)
+      .get('/contact')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          done(err); // Call done with the error if there's an issue
+          return;
+        }
+        done();
+      });
+  });
+});
+
+describe('POST /contact', () => {
+  it('should return json and correct data structure', (done) => {
+    request(app)
+      .post('/contact')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .expect({
+        "firstname": "Anna",
+        "lastname": "Andersson",
+        "email": "anna.andersson@gmail.com",
+        "personalnumber": "550713-1405",
+        "address": "Utvecklargatan 12",
+        "zipCode": "111 22",
+        "city": "Stockholm",
+        "country": "Sweden"
+      })
+      .end((err, res) => {
+        if (err) {
+          done(err);
+          return;
+        }
+        done();
+      });
+  });
 });
